2062dce6b408f7b3acdd0e100812caa9f6e6d0b2
Matthias Kausl <sig@segv.at>
2019-11-21 22:32:54 +0100

    moved login to popup

--- app/src/App.js
+++ app/src/App.js
@@ -7,9 +7,9 @@ import {
   HashRouter as Router,
   Switch,
   Route,
- withRouter
+  withRouter
 } from "react-router-dom";
-import * as querystring from 'querystring'
+
 
 import SecurityContext from './SecurityContext'
 
@@ -17,51 +17,52 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    var hash = querystring.parse(this.props.location.pathname);
 
-    if (hash['/id_token']) {
-      this.state = {
-        loggedIn: true,
-        accessToken: hash['/id_token']
-      }
-      sessionStorage.setItem("accessToken",this.state.accessToken)
-      this.props.history.push("/work")
-    } else if(sessionStorage.getItem("accessToken")){
+   if (sessionStorage.getItem("accessToken")) {
       this.state = {
         loggedIn: true,
         accessToken: sessionStorage.getItem("accessToken")
       }
-    } 
+    }
     else {
       this.state = {
         loggedIn: false
       }
       this.props.history.push("/login")
     }
-    
 
   }
 
+  onLoggedIn= (accessToken) =>{
+    console.log(accessToken)
+    this.setState({
+      loggedIn: true,
+      accessToken: accessToken
+    })
+    sessionStorage.setItem("accessToken", this.state.accessToken)
+    this.props.history.push("/work")
+  }
+
   render() {
-   
+
     return (
       <div>
-       
+
         <LinkBar loggedIn={this.state.loggedIn} />
         <div>
-        <SecurityContext.Provider value={this.state.accessToken}>
-          <Switch>
-            <Route path="/login">
-              <LoginForm onLoggedIn={this.onLoggedIn} />
-            </Route>
-            <Route path="/door_tag">
-              <ShowItems title="ruuvi tag" eventType="door_tag" key="door" />
-            </Route>
-            <Route path="/work">
-               <ShowWorkItems/>
-            </Route>
+          <SecurityContext.Provider value={this.state.accessToken}>
+            <Switch>
+              <Route path="/login">
+                <LoginForm onLoggedIn={this.onLoggedIn} />
+              </Route>
+              <Route path="/door_tag">
+                <ShowItems title="ruuvi tag" eventType="door_tag" key="door" />
+              </Route>
+              <Route path="/work">
+                <ShowWorkItems />
+              </Route>
 
-          </Switch>
+            </Switch>
           </SecurityContext.Provider>
         </div>
       </div>
--- app/src/LoginForm.js
+++ app/src/LoginForm.js
@@ -18,22 +18,36 @@ const Login = styled.div`
   
 `
 
+
+
 class LoginForm extends Component {
 
- auth_params = {
+
+  auth_params = {
     client_id: "56e5k087pt0ukianqubsgn0l91",
-    redirect_uri: /**"http://localhost:3000/",*/"https://d3vsgzl9kzxq91.cloudfront.net/",
+    redirect_uri: "https://d3vsgzl9kzxq91.cloudfront.net/login.html",
     response_type: "token",
     nonce: Math.random(),
     scope: "openid profile email"
   }
-  
+
+  url = "https://sig-segv-at.auth.eu-west-1.amazoncognito.com/login?" +
+    querystring.stringify(this.auth_params);
+
+  onLoginClick =  (e)=> {
+    console.log(e);
+    let w = window.open(this.url, "login")
+    window.addEventListener("message", (m)=>{
+      this.props.onLoggedIn(m.data)
+      w.close()
+    }, false);
+    e.preventDefault();
+  }
 
   render() {
     return (
       <Login>
-        <LoginLink href={"https://sig-segv-at.auth.eu-west-1.amazoncognito.com/login?" +
-        querystring.stringify(this.auth_params)}>Login</LoginLink>
+        <LoginLink onClick={this.onLoginClick}>Login</LoginLink>
       </Login>
     )
   }
